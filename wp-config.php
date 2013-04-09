93a17d343a2a7c7843bc27d1c02b6106920d1d48
Matthias <mail@matthiasbock.net>
2013-04-09 18:41:01 +0200

    config

---
+++ wp-config.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ *
+ * Secret-Keys, Sprache und ABSPATH. Mehr Informationen zur wp-config.php gibt es auf der {@link http://codex.wordpress.org/Editing_wp-config.php
+ *
+ * und die Installationsroutine (/wp-admin/install.php) aufgerufen wird.
+ * Man kann aber auch direkt in dieser Datei alle Eingaben vornehmen und sie von wp-config-sample.php in wp-config.php umbenennen und die Installation starten.
+ *
+ * @package WordPress
+ */
+
+/** Der Datenbankzeichensatz der beim Erstellen der Datenbanktabellen verwendet werden soll */
+define('DB_CHARSET', 'utf8');
+
+define('DB_COLLATE', '');
+
+include_once("secrets.php");
+
+/**
+ *
+ *  verschiedene WordPress-Installationen betreiben. Nur Zahlen, Buchstaben und Unterstriche bitte!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * WordPress Sprachdatei
+ *
+ * Hier kannst du einstellen, welche Sprachdatei benutzt werden soll. Die entsprechende
+ * Sprachdatei muss im Ordner wp-content/languages vorhanden sein, beispielsweise de_DE.mo
+ */
+define('WPLANG', 'de_DE');
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ */
+define('WP_DEBUG', false);
+
+/* That's all, stop editing! Happy blogging. */
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
